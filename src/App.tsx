bec846263ecfd7c7bc19255b6efdbebc1b9a0814
Dmytro Solohub <dmytro@Dmytros-MacBook-Pro.local>
2022-01-26 16:14:05 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Notes Application
+        </h1>
+        <p className="subtitle">
+          My first application with <strong>React JS</strong>!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default App;
