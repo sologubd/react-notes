1b5287e67de84668293c3f64731df2f1ac1ba43c
Dmytro <sologub.da@gmail.com>
2022-01-31 13:03:47 +0200

    apply prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -7,47 +7,52 @@ import { NotesNavbar } from "./components/navigation";
 import { Mode, INote } from "./types";
 import { getNotes, saveNotes } from "./note-model";
 
-
 const App: React.FC = ({}) => {
   const [notes, setNotes] = useState<INote[]>(getNotes());
   const [mode, setMode] = useState<Mode>(Mode.VIEW);
-  const [selectedNoteId, setSelectedNoteId] = useState<number | null>(notes.length > 0 ? 0 : null );
+  const [selectedNoteId, setSelectedNoteId] = useState<number | null>(
+    notes.length > 0 ? 0 : null
+  );
 
-  const note = (selectedNoteId !== null) ? notes[selectedNoteId] : {title: "Welcome to Web Notes App!", text:""};
+  const note =
+    selectedNoteId !== null
+      ? notes[selectedNoteId]
+      : { title: "Welcome to Web Notes App!", text: "" };
   const setViewMode = () => setMode(Mode.VIEW);
   const addNote = (note: INote) => {
     const updatedNotes = [note, ...notes];
     setSelectedNoteId(0);
-    setNotes(updatedNotes)
+    setNotes(updatedNotes);
     saveNotes(updatedNotes);
-  }
+  };
   const removeNote = () => {
-    if (selectedNoteId !== null){
+    if (selectedNoteId !== null) {
       const updateNotes = [...notes];
       updateNotes.splice(selectedNoteId, 1);
-      (updateNotes.length === 0) ? setSelectedNoteId(null) : setSelectedNoteId(0);
+      updateNotes.length === 0 ? setSelectedNoteId(null) : setSelectedNoteId(0);
       saveNotes(updateNotes);
       setNotes(updateNotes);
     }
-  }
+  };
 
   return (
-    <div className="columns">
-      <div className="column">
-        <NotesNavbar setMode={setMode} removeNote={removeNote}/>
-        <NotesList notes={notes} showNote={setSelectedNoteId} selectedNoteId={selectedNoteId} />
+    <div className='columns'>
+      <div className='column'>
+        <NotesNavbar setMode={setMode} removeNote={removeNote} />
+        <NotesList
+          notes={notes}
+          showNote={setSelectedNoteId}
+          selectedNoteId={selectedNoteId}
+        />
       </div>
-      <div className="column is-three-quarters content">
-        {(mode === Mode.VIEW) && <Note note={note} />}
-        {(mode === Mode.EDIT) && <NoteForm 
-                                    goToMainView={setViewMode}
-                                    note={note}
-                                    addNote={addNote}
-                                  />
-        }
+      <div className='column is-three-quarters content'>
+        {mode === Mode.VIEW && <Note note={note} />}
+        {mode === Mode.EDIT && (
+          <NoteForm goToMainView={setViewMode} note={note} addNote={addNote} />
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
